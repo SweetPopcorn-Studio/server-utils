449c755b9fe4906b57af0adb9337e22e4046b7c5
Simon <97951171+SweetpopcornSimon@users.noreply.github.com>
2022-03-03 11:35:35 +0100

    Update server-setup.sh

--- server-setup.sh
+++ server-setup.sh
@@ -10,6 +10,10 @@ sudo apt update
 sudo apt install git
 printf '\nGIT installed successfully\n\n'
 
+# Necessary Tools 
+sudo apt-get install software-properties-common
+sudo apt-get install -y gnupg2
+
 # Docker
 sudo apt remove --yes docker docker-engine docker.io containerd runc || true
 sudo apt update
@@ -26,9 +30,10 @@ printf 'Waiting for Docker to start...\n\n'
 sleep 5
 
 # Docker Compose
-sudo wget --output-document=/usr/local/bin/docker-compose "https://github.com/docker/compose/releases/download/$(wget --quiet --output-document=- https://api.github.com/repos/docker/compose/releases/latest | grep --perl-regexp --only-matching '"tag_name": "\K.*?(?=")')/run.sh"
+sudo wget "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-sudo wget --output-document=/etc/bash_completion.d/docker-compose "https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose"
+sleep 1
+docker-compose --version
 printf '\nDocker Compose installed successfully\n\n'
 
 # Certbot
